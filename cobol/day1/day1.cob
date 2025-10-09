ed45decaaf7441f8cc051c9339e50e7d7912360c
Dj-Viking <anders.swedishviking@gmail.com>
2025-10-09 19:21:55 -0400

    update vars for reading file into split string as array

--- cobol/day1/day1.cob
+++ cobol/day1/day1.cob
@@ -8,24 +8,32 @@
                    assign to "../sample",
                    organization is line sequential,
                    file status is filestatus.
+
            DATA DIVISION.
            FILE SECTION.
-      *>       684 characters in one line 
-      *>       plus null terminating char
            FD sample
                record contains 685 characters.
            01 sampleline pic x(685).
-           WORKING-STORAGE SECTION.
-           01 eof-check       pic X value "N". 
-           01 filestatus      pic XX.
-           01 answer          pic 9 value zero. 
-           PROCEDURE DIVISION.
 
+           WORKING-STORAGE SECTION.
+           01 filestatus    pic xx.
+           01 ws-delim      pic X    value ",".
+           01 ws-len        pic 9(4) value 0.
+           01 ws-start      pic 9(4) value 0.
+           01 ws-end        pic 9(4) value 0.
+           01 ws-count      pic 9(4) value 0.
+           01 ws-char       pic x. 
+           01 ws-field-len  pic 9(4) value 0.
+           01 ws-table.
+              05 ws-item occurs 10 times.
+                 10 ws-text pic x(40). 
+
+           PROCEDURE DIVISION.
           *> cobol-lint CL002 main-program
            main-program.
                perform init-file
                
-               perform read-file
+               perform read-file-to-array
 
                perform close-file
 
@@ -36,21 +44,24 @@
                    display "error opening file. status=" filestatus
                    stop run
                end-if
+               read sample
+                   at end
+                       display "no data in file."
+                       close sample
+                       stop run
+               end-read
                .
 
-      *> do something here per character
-      *> of the line, maybe use a temp string
-      *> to check what each set of 3 characters are
-      *> and add up how far we went 
-           read-file.
-               perform until eof-check = "Y"
-                   read sample
-                       at end
-                           move "Y" to eof-check
-                       not at end
-                           display sampleline
-                   end-read
-               end-perform
+           read-file-to-array.
+               display "test"
+      *>         perform until eof-check = "Y"
+      *>             read sample
+      *>                 at end
+      *>                     move "Y" to eof-check
+      *>                 not at end
+      *>                     display sampleline
+      *>             end-read
+      *>         end-perform
                .
 
            close-file.
