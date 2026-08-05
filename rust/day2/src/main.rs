a77432f910062d063697630554803c167ea820da
Dj-Viking <anders.swedishviking@gmail.com>
2026-08-05 00:02:06 -0400

    solve day2 part2

--- rust/day2/src/main.rs
+++ rust/day2/src/main.rs
@@ -8,8 +8,138 @@ const UP:    (i8, i8) = (-1, 0);
 const DOWN:  (i8, i8) = (1 , 0);
 
 fn main() {
+	part1();
+	part2();
+}
 
-	// part1
+fn part2() {
+
+	// starting point is at 5 on the new keypad
+
+	let mut position: (usize, usize) = (2, 0);
+
+	// let instructions = read_to_string("./sampleinstructions")
+	// 	.unwrap();
+	let instructions = read_to_string("./input")
+		.unwrap();
+
+	let dirs = instructions
+		.split("\n")
+		.filter(|x| !x.is_empty())
+		.collect::<Vec<_>>();
+
+	let grid_file = read_to_string("./samplekeypad2")
+		.unwrap();
+
+	let mut grid = grid_file
+		.split("\n")
+			.map(|l| l.split(" ")
+					 .map(|x| x.to_string())
+				     .collect::<Vec<_>>()
+			)
+		.collect::<Vec<_>>();
+	grid.pop();
+
+	// printgrid2(&grid);
+
+	// highlight starting point
+	let grid_with_start = "[".to_string() + &grid[position.0][position.1] + "]";
+	grid[position.0][position.1] = grid_with_start; 
+	
+
+	let mut input = String::new();
+
+	// each line of directions indicates the number that the grid ends up on from the
+	// starting point for each line of directions
+	let mut passkey = String::new();
+
+	for row in 0..dirs.len() {
+		// println!("dirs row is {:?}", dirs[row]);
+		let chars = dirs[row].chars().collect::<Vec<_>>();
+		for col in 0..chars.len() {
+			let direction_to_match = String::new() + &chars[col].to_string();
+			// println!("direction is [{:?}]", direction_to_match);
+			let current_direction = match direction_to_match.as_str() {
+				"U" => { UP    },
+				"D" => { DOWN  },
+				"L" => { LEFT  },
+				"R" => { RIGHT },
+				_   => { panic!("unreachable reached...invalid direction");}
+			};
+
+			// println!("current direction: {:?}", current_direction);
+
+
+			// reset current position
+			grid[position.0][position.1] = grid[position.0][position.1].replace("[", "").replace("]", "");
+
+			// println!("grid before **");
+			// printgrid2(&grid);
+
+			// update current position according to current direction
+
+			let previous_pos_zero = position.0.clone();
+			position.0 = {
+				if ((position.0 as i8) + current_direction.0) < 0 
+				|| ((position.0 as i8) + current_direction.0) > ((grid.len() as i8) - 1)
+				{
+					position.0
+				} else {
+					<i8 as TryInto<usize>>::try_into((position.0 as i8) + current_direction.0).unwrap() as usize
+				}
+			};
+
+			let previous_pos_one = position.1.clone();
+			position.1 = {
+				if ((position.1 as i8) + current_direction.1 < 0) 
+				|| ((position.1 as i8) + current_direction.1) > ((grid[0].len() as i8) - 1) 
+				{
+					position.1
+				} else {
+					<i8 as TryInto<usize>>::try_into((position.1 as i8) + current_direction.1).unwrap() as usize
+				}
+			};
+
+			// hmm
+			// println!("pos now {:?}", position);
+			// println!("previous0 {:?}", previous_pos_zero);
+			// println!("previous1 {:?}", previous_pos_one);
+			if grid[position.0][position.1].is_empty() {
+				position.0 = previous_pos_zero;
+				position.1 = previous_pos_one;
+			}
+
+			grid[position.0][position.1] = "[".to_string() + &grid[position.0][position.1] + "]";
+			
+			// printgrid2(&grid);
+			
+			// if on the last instruction of the instruction row
+			// then update the passkey to the number that was last arrived to for the last
+			// instruction
+
+			// wait for enter key
+			// println!("press enter to continue");
+			// match io::stdin().read_line(&mut input) {
+			// 	Ok(n)  => { println!("================================================================"); },
+			// 	Err(e) => { println!("error reading input: {:?}", e)}
+			// }
+		}
+		// update passcode here at the end of chars len in instruction row
+		passkey = passkey + &(grid[position.0][position.1].replace("[", "").replace("]", ""));
+		// println!("***** passkey updated {}", passkey);
+	}
+	println!("part2: {}", passkey);
+}
+
+fn printgrid2(grid: &Vec<Vec<String>>) {
+	println!("     {:?}", grid[0]);
+	println!("    {:?}", grid[1]);
+	println!("   {:?}", grid[2]);
+	println!("    {:?}", grid[3]);
+	println!("     {:?}", grid[4]);
+}
+
+fn part1() {
 	let grid_file = read_to_string("./samplekeypad")
 		.unwrap();
 
@@ -20,7 +150,7 @@ fn main() {
 	let instructions = read_to_string("./input")
 		.unwrap();
 
-	// todo: this needs to still be rows of letters for directions to the next number of the door
+	// this needs to still be rows of letters for directions to the next number of the door
 	// code 
 	let dirs = instructions
 		.split("\n")
@@ -92,7 +222,7 @@ fn main() {
 			// 	println!("{:?}",grid[i]);
 			// }
 			
-			//todo: if on the last instruction of the instruction row
+			// if on the last instruction of the instruction row
 			// then update the passkey to the number that was last arrived to for the last
 			// instruction
 
@@ -108,7 +238,4 @@ fn main() {
 		// println!("***** passkey updated {}", passkey);
 	}
 	println!("part1: {}", passkey);
-	// part 1
-
 }
-
